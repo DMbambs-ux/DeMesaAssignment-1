d904176a4d2fd1891113242afe4d79de1badfb9c
BambooDeMesa <demesabamboogeof@gmail.com>
2026-08-17 15:09:14 +0800

    Add files via upload

---
+++ If..Elseif...Else Statement.php
@@ -0,0 +1,12 @@
+<?php
+
+$Age = 80;
+
+if ($Age < 18){
+    echo "This person is a minor.";
+} elseif ($Age <= 64){
+    echo "This person is an Adult.";
+} else {
+    echo "This person is a Senior.";
+}
+?>
